56163615f07fe06a17d64e501fdf91855be4f308
Thomas Battram <thomas.battram@bristol.ac.uk>
2021-07-01 14:18:02 +0100

    modularised and added plots

--- scripts/test-methods-results.R
+++ scripts/test-methods-results.R
@@ -15,37 +15,32 @@ library(usefunc) # own package of useful functions
 res_dir <- "results/temp"
 res_files <- list.files(res_dir, full.names = T)
 
-tca_files <- grep("tca", res_files, value = T)
+methods <- c("tca", "celldmc", "tcareg", "toast", "omicwas") 
 
-tca_res <- map_dfr(tca_files, function(file) {
-	res <- new_load(file)
-	return(bind_rows(res, .id = "phenotype"))
-})
+bind_data <- function(method, res_files)
+{
+	filenames <- grep(paste0(method, "_res"), res_files, value = T)
+	message("There are ", length(filenames), " files for the ", method, " method.")
+	out_res <- map_dfr(filenames, function(file) {
+		res <- new_load(file)
+		return(bind_rows(res, .id = "phenotype"))
+	})
+	return(out_res)
+}
 
-celldmc_files <- grep("cell", res_files, value = T)
-
-celldmc_res <- map_dfr(celldmc_files, function(file) {
-	res <- new_load(file)
-	return(bind_rows(res, .id = "phenotype"))
-})
-
+list_res <- lapply(methods, bind_data, res_files=res_files)
+names(list_res) <- methods
+all_res <- bind_rows(list_res, .id = "method")
 # TEST 2!!
 # tca_res <- new_load("results/tca_res_50cpgs.RData")
 # tca_res <- bind_rows(tca_res, .id = "phenotype")
 # celldmc_res <- new_load("results/celldmc_res_50cpgs.RData")
 # celldmc_res <- bind_rows(celldmc_res, .id = "phenotype")
 
-summary(tca_res)
-summary(celldmc_res)
-
-tca_res$method <- "TCA"
-celldmc_res$method <- "celldmc"
-all_res <- bind_rows(tca_res, celldmc_res)
-
 ## plots
 celltypes <- unique(all_res$cell)
 
-pal <- get_cb_palette()[1:2]
+pal <- get_cb_palette()[1:length(unique(all_res$method))]
 fdr_plot_list <- lapply(celltypes, function(ct) {
 	ct_res <- all_res[all_res$cell == ct, ]
 	ggplot(ct_res, aes(x = phenotype, y = n_sig, colour = method)) +
@@ -61,9 +56,23 @@ leg <- get_legend(fdr_plot_list[[1]])
 
 fdr_plot_list <- lapply(fdr_plot_list, function(p) p + theme(legend.position = "none"))
 
-prow <- plot_grid(plotlist = fdr_plot_list, nrow = 3, 
+prow <- plot_grid(plotlist = fdr_plot_list, nrow = length(celltypes)/2, 
 				  labels = celltypes)
 
 out_plots <- plot_grid(prow, leg, ncol = 2, rel_widths = c(3, .4))
 ggsave("results/celldmc-tca-false-positives.pdf", plot = out_plots)
 # ggsave("results/celldmc-tca-false-positives-50cpgs.pdf", plot = out_plots)
+
+## lambda plots
+
+pal <- get_cb_palette()[1:length(unique(all_res$method))]
+p_lamb <- ggplot(all_res, aes(x = cell, y = lambda_est, fill = method)) + 
+	geom_violin() + 
+	geom_boxplot(position=position_dodge(0.9), width = 0.2) + 
+	# stat_summary(fun=median, geom="point", size=2) + 
+	labs(y = "Lambda") + 
+	scale_fill_manual(values = pal) + 
+	theme_bw()
+ggsave("results/celldmc-tca-lambdas.pdf", plot = p_lamb)
+
+
